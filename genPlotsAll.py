abb1b0e05b23387e39dec913045801c281a7622c
noahfranz13 <noahfranz13@gmail.com>
2022-03-04 09:47:56 -0500

    Make the figure size better

--- genPlotsAll.py
+++ genPlotsAll.py
@@ -50,7 +50,7 @@ def plotWfSep(satCsv, h5Path, memLim=20):
 
     fig.savefig(f"{targetName}_{satName.replace(' ','_')}_wf.png", bbox_inches='tight', transparent=False)'''
 
-    plt.figure()
+    plt.figure(figsize=(13, 10))
     wf.plot_all()
     plt.savefig(f"{targetName}_{satName.replace(' ','_')}_wf.png", bbox_inches='tight', transparent=False)
 
